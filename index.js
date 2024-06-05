d3d3e6865c46b56e3a7e71f0769a25c3cc40f658
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2024-06-05 15:27:08 -0300

    Cleaning and refactoring

--- index.js
+++ index.js
@@ -10,11 +10,9 @@ const io = socketIo(server, {
 });
 
 let rooms = [];
-const roomLastActivity = {};
 let sentCards = {};
-let playerChoices = {}; // Track player choices per room
-let choiceTimeouts = {};
-const roomPlayersCount = {};
+let playerChoices = {} //player choices per room
+const roomLastActivity = {}; //each room last activity
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,29 +21,26 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
-
     socket.emit('roomsList', rooms);
 
-    socket.on('createRoom', (roomName) => {
+    socket.on('roomCreated', (roomName) => {
         if (!rooms.includes(roomName)) {
             rooms.push(roomName);
             roomLastActivity[roomName] = Date.now();
-            sentCards[roomName] = {};
             playerChoices[roomName] = {};
-            roomPlayersCount[roomName] = 0;
             io.emit('roomsList', rooms);
-            console.log(`Room created: ${roomName}`);
+            // console.log(`Room created: ${roomName}`);
         } else {
+            // io.emit("roomExists", roomName);
             console.log(`Room already exists: ${roomName}`);
         }
     });
 
-    socket.on('joinRoom', async (room) => {
+        socket.on('joinRoom', async (room) => {
         if (rooms.includes(room)) {
 
             let size = await io.in(room).allSockets();
-            console.log(size.size);
+            // console.log(size.size);
 
             if (size.size <= 2){
                 socket.join(room);
@@ -56,14 +51,22 @@ io.on('connection', (socket) => {
 
                 // Update last activity time when user joins
                 roomLastActivity[room] = Date.now();
-            } else {
-                console.log("I WON'T ALLOW IT!");
-            }
 
+
+
+            } else {
+                io.to(room).emit('message', 'This room is full.')
+            }
 
         } else {
             console.log(`Attempted to join non-existent room: ${room}`);
         }
+    });
+
+    socket.on('leave', (room) => {
+        console.log(`Player ${socket.id} left the room ${room}`)
+        // Emit a 'leaveRoom' event to all other players in the room
+        socket.to(room).emit('leaveRoom', socket.id, room);
     });
 
     socket.on('sendNumber', async (number, room) => {
@@ -78,7 +81,7 @@ io.on('connection', (socket) => {
                 sentCards[room] = {};
             }
             if (!sentCards[room][playerId]) {
-                        // Generate cards for the player and send them to the player   ]
+                // Generate cards for the player and send them to the player   ]
                 const cards = generateCards(number, playerId);
                 io.to(playerId).emit('playerCards', cards, playerId);
                 console.log("sent cards");
@@ -87,6 +90,7 @@ io.on('connection', (socket) => {
         }else {
                 console.log("Room is full. Cannot send number.");
         }
+    });
 
             // if (roomPlayersCount[room] < 2) { // Check if room is not full
             //     if (!sentCards[room]) {
@@ -115,69 +119,40 @@ io.on('connection', (socket) => {
             //     console.log("sent cards");
             //     sentCards[room][playerId] = true;
             // }
+        //
+        //         });
 
-        });
+    socket.on('cardChosen', (card, room) => {
 
-        socket.on('cardChosen', (card, room) => {
+        const playerId = socket.id;
 
-            const playerId = socket.id;
+        if (!room) {
+            console.error('Player is not in any room.');
+            return;
+        }
 
-            if (!room) {
-                console.error('Player is not in any room.');
-                return;
-            }
-
-            if (!playerChoices[room]) {
-                playerChoices[room] = {};
-            }
-
-
-            playerChoices[room][playerId] = card;
-            roomLastActivity[room] = Date.now();
+        if (!playerChoices[room]) {
+            playerChoices[room] = {};
+        }
 
 
-            // Check if both players have chosen
-            if (Object.keys(playerChoices[room]).length === 2) {
-                clearTimeout(choiceTimeouts[room]); // Clear timeout if both players have chosen
+        playerChoices[room][playerId] = card;
+        roomLastActivity[room] = Date.now();
+
+
+        // Check if both players have chosen
+        if (Object.keys(playerChoices[room]).length === 2) {
+            clearTimeout(choiceTimeouts[room]); // Clear timeout if both players have chosen
+            revealChoices(room);
+        } else if (!choiceTimeouts[room]) {
+            // Set timeout for 30 seconds
+            choiceTimeouts[room] = setTimeout(() => {
                 revealChoices(room);
-            } else if (!choiceTimeouts[room]) {
-                // Set timeout for 30 seconds
-                choiceTimeouts[room] = setTimeout(() => {
-                    revealChoices(room);
-                }, 30000);
-            }
-        });
-
-        socket.on('leave', () => {
-            // Emit a custom event to alert the remaining player
-            // socket.emit('playerLeft', 'Someone left the game');
-        });
-
-        socket.on('disconnect', () => {
-            console.log('User disconnected:', socket.id);
-
-            rooms = rooms.filter((room) => !Object.keys(io.sockets.adapter.rooms).includes(room));
-
-            io.emit('roomsList', rooms);
-        });
-
-
+            }, 30000);
+        }
     });
 
-// Function to check room activity and perform cleanup
-function checkRoomActivity() {
-    console.log("checking room activity...")
-    const now = Date.now();
-    for (const room in roomLastActivity) {
-        if (now - roomLastActivity[room] > 5 * 60 * 1000) { // Check if inactive for 5 minutes
-            // Perform cleanup or action for inactive room
-            console.log(`Room ${room} has been inactive.`);
-            rooms.pop();
-            delete roomLastActivity[room];
-            // Additional cleanup logic here
-        }
-    }
-}
+});
 
 function generateCards(num, playerId) {
     const cards = [];
@@ -261,12 +236,7 @@ function revealChoices(room) {
     // Reset choices and timeout
     playerChoices[room] = {};
     delete choiceTimeouts[room];
-}
-
-
-
-// Check room activity every minute
-setInterval(checkRoomActivity, 60 * 1000);
+} 
 
 server.listen(port, () => {
     console.log('Listening on *: ', port);
